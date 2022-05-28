81fef1f14f55c2858649ead0bbcc0eef84dac4f3
Doyinsola Egbewande <61497782+egbewandedoyinsola@users.noreply.github.com>
2022-05-28 17:06:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-# Read text from a file, and count the occurence of words in that text
+# This simple python program reads text from a file, and count the occurence of words in that text
 # Example:
 # count_words("The cake is done. It is a big cake!")
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
-# put this comment after
-# openfile = open("./story.txt", "r") this or the line immediately after
-# will open the file.
+
 
 def read_file_content(filename):
     # [assignment] Add your code here
